1c9ae6d31fd436c5d2778f2c83a896e6a2f20bf7
memu <mkmet@ya.ru>
2018-09-22 20:22:09 +0500

    main and test progect assemblies setup

---
+++ unity/Assets/Tests/CoreTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using SucoSnake.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellFieldTest {
+
+	[Test]
+	public void Start () {
+		var field = new CellFieldUnit(10, 10);
+		field.Initialize();
+		var cell = field.GetCell(5, 5);
+		Assert.AreEqual(cell.X, 5);
+	}
+}
